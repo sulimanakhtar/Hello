1e68c4276f630e5af81107b13b7239940f36423a
sulimanakhtar <makhtar@planittesting.com>
2018-09-13 11:56:33 +0100

    HelloWorld is added

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+ Volume in drive C is Windows
+ Volume Serial Number is BA2A-5B66
+
+ Directory of C:\Users\makhtar\Downloads\Data\Genkins
+
+13/09/2018  11:55    <DIR>          .
+13/09/2018  11:55    <DIR>          ..
+12/09/2018  16:24               192 Hello.java
+13/09/2018  11:55                 0 HelloWorld.java
+12/09/2018  15:57                 0 index.html
+               3 File(s)            192 bytes
+               2 Dir(s)  149,361,778,688 bytes free
